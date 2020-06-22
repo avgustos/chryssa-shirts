fbd142fd912490f7a2ec4ac0787a53a504c858e8
avgustos <36605918+avgustos@users.noreply.github.com>
2020-06-22 15:03:19 +0300

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,21 @@
+<?php
+	$first_name = $_POST['first-name'];
+	$last_name = $_POST['last-name'];
+	$client_email = $_POST['email'];
+	$client_phone = $_POST['phone'];
+	$message = $_POST['message'];
+	
+	$email_from = "z95463chry@chryssa-shirts.gr";
+	$email_subject = "Ήρθε Ένα Νέο Μήνυμα Από Σαρδέλλα";
+	$email_body = "Όνομα Σαρδέλλας: $first_name.\n".
+				  "Επώνυμο Σαρδέλλας: $last_name.\n".	
+     			  "Email Σαρδέλλας: $client_email.\n".
+				  "Τηλέφωνο Σαρδέλλας: $client_phone.\n".
+				  "Το μήνυμα της Σαρδέλλας: $message.\n";
+	$to = "contact@chryssa-shirts.gr";
+	$headers = "Αποστολέας: $email_from\r\n";
+	$headers = "Απάντηση στο: $client_email\r\n";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	header("Location: index.html");
+?>
